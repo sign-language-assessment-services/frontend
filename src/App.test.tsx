9d56519252160c019bbed46b35994b62ca3fad9f
Dustin Heckmann <dustin.heckmann@thoughtworks.com>
2020-06-13 16:08:09 +0200

    Display only "Hello, world!"
    #1

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react';
+import { App } from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('shows hello world', () => {
+    render(<App />);
+    expect(screen.getByText(/Hello, world!/)).toBeInTheDocument();
+  })
 });
