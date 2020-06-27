29ccb016d7d0ece69887a4da77e00dcf9cd7eaea
Dustin Heckmann <kontakt@dustin-heckmann.de>
2020-06-27 19:54:38 +0200

    Proxy API requests to localhost:8000 for local development #5

---
+++ src/setupProxy.js
@@ -0,0 +1,15 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+// Forwards requests to "/api" to "localhost:8000/" for local development
+// Requires that the backend is running locally and is listening on port 8000
+
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://localhost:8000",
+      changeOrigin: true,
+      pathRewrite: { "^/api": "" },
+    })
+  );
+};
