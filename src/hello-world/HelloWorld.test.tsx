fe5f5dae1ecf9987bd43c355cbb7b1976a786468
Dustin Heckmann <kontakt@dustin-heckmann.de>
2020-06-27 20:15:59 +0200

    Get Hello World from backend #5

--- src/hello-world/HelloWorld.test.tsx
+++ src/hello-world/HelloWorld.test.tsx
@@ -1,10 +1,45 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, wait } from "@testing-library/react";
 import { HelloWorld } from "./HelloWorld";
+import { getHelloWorldMessage } from "./helloWorldService";
+
+jest.mock("./helloWorldService");
 
 describe("HelloWorld", () => {
-  it("shows hello world", () => {
+  it("shows hello world", async () => {
+    // given
+    (getHelloWorldMessage as jest.Mock).mockResolvedValue(
+      "Hello Mocked World!"
+    );
+
+    // when
     render(<HelloWorld />);
-    expect(screen.getByText(/Hello world!/)).toBeInTheDocument();
+
+    // then
+    await wait(() =>
+      expect(screen.getByText(/Hello Mocked World!/)).toBeInTheDocument()
+    );
+  });
+
+  it("shows error message when request for hello world message fails", async () => {
+    // given
+    (getHelloWorldMessage as jest.Mock).mockRejectedValue(
+      new Error("mocked error")
+    );
+
+    // when
+    render(<HelloWorld />);
+
+    // then
+    await wait(() => expect(screen.getByText(/error/i)).toBeInTheDocument());
+  });
+
+  it("shows loading message while request is pending", async () => {
+    // when
+    render(<HelloWorld />);
+
+    // then
+    expect(screen.getByText(/loading/i)).toBeInTheDocument();
+    await wait(() => expect(getHelloWorldMessage).toHaveBeenCalled());
   });
 });
--- src/hello-world/HelloWorld.tsx
+++ src/hello-world/HelloWorld.tsx
@@ -1,3 +1,14 @@
 import React from "react";
+import { useAsync } from "react-use";
+import { getHelloWorldMessage } from "./helloWorldService";
 
-export const HelloWorld = (): JSX.Element => <>Hello world!</>;
+export const HelloWorld = (): JSX.Element => {
+  const { loading, value: message, error } = useAsync(getHelloWorldMessage, []);
+  return (
+    <>
+      {loading ? "Loading..." : null}
+      {message ? message : null}
+      {error ? "Error: Failed to load 'Hello World!' from backend." : null}
+    </>
+  );
+};
