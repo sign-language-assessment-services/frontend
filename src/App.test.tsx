32b168cd73baf097d15ec1f93853e8b5b0d0b5e1
Dustin Heckmann <kontakt@dustin-heckmann.de>
2020-06-28 11:14:29 +0200

    Reformat code with Prettier
    use single quotes where possible,
    avoid semicolons,
    add trailing commas,

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import { App } from "./App";
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { App } from './App'
 
 // Mock HelloWorld component so that we can assert it is rendered
-jest.mock("./hello-world", () => ({
+jest.mock('./hello-world', () => ({
   HelloWorld: function HelloWorld() {
-    return <span data-testid="HelloWorld" />;
+    return <span data-testid="HelloWorld" />
   },
-}));
+}))
 
-describe("App", () => {
-  it("includes HelloWorld component", () => {
-    render(<App />);
-    expect(screen.getByTestId("HelloWorld")).toBeInTheDocument();
-  });
-});
+describe('App', () => {
+  it('includes HelloWorld component', () => {
+    render(<App />)
+    expect(screen.getByTestId('HelloWorld')).toBeInTheDocument()
+  })
+})
--- src/hello-world/types.ts
+++ src/hello-world/types.ts
@@ -1,3 +1,3 @@
 export interface HelloWorldResponse {
-  msg: string;
+  msg: string
 }
