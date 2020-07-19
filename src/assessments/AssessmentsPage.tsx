32b4d573419b365dbab8565b9bc07fe4d90afad0
Dustin Heckmann <dustin@MBP-de-Dustin.fritz.box>
2020-07-19 18:34:34 +0200

    Show assessment info from backend

--- src/assessments/AssessmentsPage.tsx
+++ src/assessments/AssessmentsPage.tsx
@@ -1,3 +1,33 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useState, useEffect } from 'react'
+import { Assessment } from './models'
+import { getAssessmentById } from './assessmentsService'
 
-export const AssessmentsPage = (): ReactElement => <div>AssessmentsPage</div>
+export const AssessmentsPage = (): ReactElement | null => {
+  const [assessment, setAssessment] = useState<Assessment>()
+  const fetchAssessment = async () => {
+    const assessment = await getAssessmentById('1')
+    setAssessment(assessment)
+  }
+  useEffect(() => {
+    fetchAssessment()
+  }, [])
+
+  if (assessment) {
+    return (
+      <>
+        <h1>{assessment.name}</h1>
+        {assessment.items.map((item, index) => (
+          <div key={index}>
+            <h3>{item.description}</h3>
+            <ul>
+              {item.choices.map((choice, index) => (
+                <li key={index}>{choice.label}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </>
+    )
+  }
+  return null
+}
