c6e796c2ffed971e72e230ab94683919b8cc01fe
Dustin Heckmann <kontakt@dustin-heckmann.de>
2020-10-03 16:15:51 +0200

    Add service method for submitting a solution

--- src/assessments/assessmentsService.ts
+++ src/assessments/assessmentsService.ts
@@ -1,7 +1,18 @@
 import axios, { AxiosResponse } from 'axios'
-import { Assessment } from './models'
+import { Assessment, ScoringResult, Submission } from './models'
 
 export const getAssessmentById = async (id: string): Promise<Assessment> => {
-  const result: AxiosResponse<Assessment> = await axios.get(`/api/assessments/${id}`)
-  return result.data
+  const response: AxiosResponse<Assessment> = await axios.get(`/api/assessments/${id}`)
+  return response.data
+}
+
+export const scoreAssessment = async (
+  assessmentId: string,
+  submission: Submission,
+): Promise<ScoringResult> => {
+  const response: AxiosResponse<ScoringResult> = await axios.post(
+    `/api/assessments/${assessmentId}/submissions`,
+    submission,
+  )
+  return response.data
 }
