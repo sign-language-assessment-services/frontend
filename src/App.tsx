240db487b94a0f59aab3478d0dfa7ac6ef0e50e2
Dustin Heckmann <kontakt@dustin-heckmann.de>
2023-02-05 15:14:42 +0100

    Show spinner while app is loading

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,13 @@ import React, { ReactElement } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { AssessmentsPage } from './assessments'
 import PrivateRoute from './auth/PrivateRoute'
-import { useAuthentication } from './auth/useAuthentication'
-import { LoadingIndicator } from './assessments/LoadingIndicator'
 
 export const App = (): ReactElement => {
-  const { initialized } = useAuthentication()
-  if (!initialized) {
-    return <LoadingIndicator />
-  }
   return (
-    <div className="">
-      <PrivateRoute requiredRole="slas-frontend-user">
-        <Routes>
-          <Route path="/assessments/*" element={<AssessmentsPage />} />
-          <Route path="/" element={<AssessmentsPage />} />
-          <Route path="*" element={<div>Not Found</div>} />
-        </Routes>
-      </PrivateRoute>
-    </div>
+    <PrivateRoute requiredRole="slas-frontend-user">
+      <Routes>
+        <Route path="*" element={<AssessmentsPage />} />
+      </Routes>
+    </PrivateRoute>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
 
 import { createRoot } from 'react-dom/client'
 import { AuthenticationProvider } from './auth/AuthenticationProvider'
+import { LoadingIndicator } from './assessments/LoadingIndicator'
 
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container)
 
 root.render(
-  <AuthenticationProvider>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
-  </AuthenticationProvider>,
+  <Suspense fallback={<LoadingIndicator />}>
+    <AuthenticationProvider>
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
+    </AuthenticationProvider>
+  </Suspense>,
 )
