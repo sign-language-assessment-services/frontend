d837d0545a52e7d4bc1db7f89e3007e4f493c880
Dustin Heckmann <kontakt@dustin-heckmann.de>
2023-02-05 14:28:23 +0100

    Redirect to Keycloak instead of showing a Login button if not authenticated

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,14 @@ import { AssessmentsPage } from './assessments'
 import PrivateRoute from './auth/PrivateRoute'
 import { useAuthentication } from './auth/useAuthentication'
 import { LoadingIndicator } from './assessments/LoadingIndicator'
-import { LoginButton } from './components/LoginButton'
 
 export const App = (): ReactElement => {
-  const { initialized, authenticationEnabled, authenticated } = useAuthentication()
+  const { initialized } = useAuthentication()
   if (!initialized) {
     return <LoadingIndicator />
   }
   return (
     <div className="">
-      {authenticationEnabled && !authenticated ? <LoginButton /> : null}
       <PrivateRoute requiredRole="slas-frontend-user">
         <Routes>
           <Route path="/assessments/*" element={<AssessmentsPage />} />
