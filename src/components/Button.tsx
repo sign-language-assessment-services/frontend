d73158dd506f68c599e59035e0b6a96d1f2c3f3f
Dustin Heckmann <kontakt@dustin-heckmann.de>
2023-02-05 16:23:36 +0100

    Support reload icon in Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,13 @@
 import React, { ButtonHTMLAttributes } from 'react'
 import cx from 'classnames'
 
-type IconType = 'prev' | 'next'
+type IconType = 'prev' | 'next' | 'reload'
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   icon?: IconType
   iconPosition?: 'left' | 'right'
 }
+
 export const Button = ({
   children,
   icon,
@@ -61,14 +62,32 @@ export const Button = ({
 }
 
 const Icon = ({ type }: { type: IconType }) => {
-  const pathData = {
-    prev: 'M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z',
-    next: 'M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z',
+  return {
+    prev: (
+      <svg className="fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+        <path d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" />
+      </svg>
+    ),
+    next: (
+      <svg className="fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+        <path d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" />
+      </svg>
+    ),
+    reload: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={2.2}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+        />
+      </svg>
+    ),
   }[type]
-  const className = 'fill-current w-8 h-8'
-  return (
-    <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-      <path d={pathData} />
-    </svg>
-  )
 }
