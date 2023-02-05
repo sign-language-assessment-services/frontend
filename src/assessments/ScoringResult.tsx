de41e63b524a24c5e7ec117296cfeeaa449b4c61
Dustin Heckmann <kontakt@dustin-heckmann.de>
2023-02-05 16:26:25 +0100

    Style scoring result view

--- src/assessments/ScoringResult.tsx
+++ src/assessments/ScoringResult.tsx
@@ -1,13 +1,33 @@
-import { ScoringResult as ScoringResultModel } from './models'
+import { Assessment, ScoringResult as ScoringResultModel } from './models'
 
 import React, { ReactElement } from 'react'
+import { Header } from '../components/layout/Header'
+import { Footer } from '../components/layout/Footer'
+import { PageContainer } from '../components/layout/PageContainer'
+import { Main } from '../components/layout/Main'
+import { Button } from '../components/Button'
 
 interface Props {
+  assessment: Assessment
   scoringResult: ScoringResultModel
 }
 
-const ScoringResult = ({ scoringResult }: Props): ReactElement => (
-  <>Your result: {scoringResult.score}</>
+const ScoringResult = ({ assessment, scoringResult: { score } }: Props): ReactElement => (
+  <PageContainer>
+    <Header>{assessment.name}</Header>
+    <Main>
+      <div className="flex flex-col gap-6 items-center">
+        <span>Sie haben</span>
+        <span className="text-6xl font-bold">{score} Punkt(e)</span>
+        <span>erreicht</span>
+      </div>
+    </Main>
+    <Footer>
+      <Button onClick={() => window.location.reload()} icon="reload">
+        Test wiederholen
+      </Button>
+    </Footer>
+  </PageContainer>
 )
 
 export default ScoringResult
