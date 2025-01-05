d09fd797b272fb0cf70ac63b2b7fdbd34c16756e
Dustin Heckmann <kontakt@dustin-heckmann.de>
2025-01-05 09:57:40 +0100

    Redirect user to current page after login

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,14 @@ import { signIn, auth } from '@/lib/auth'
 
 export default auth(async (req) => {
   if (!req.auth && !req.nextUrl.pathname.startsWith('/api/auth')) {
-    const url = await getKeycloakSignInUrl()
+    const url = await getKeycloakSignInUrl(req.url)
     return Response.redirect(url)
   }
 })
 
-async function getKeycloakSignInUrl() {
+async function getKeycloakSignInUrl(url: string) {
   'use server'
-  return await signIn('keycloak', { redirect: false })
+  return await signIn('keycloak', { redirect: false, redirectTo: url })
 }
 
 export const config = {
