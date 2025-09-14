2ae600c81162307edbdc706e0b97ab6497810dd1
Dustin Heckmann <kontakt@dustin-heckmann.de>
2025-09-14 21:45:38 +0200

    Use <img> instead of <Image> tag

--- app/assessments/[assessmentId]/[submissionId]/[taskId]/_components/multimedia/MultimediaComponent.tsx
+++ app/assessments/[assessmentId]/[submissionId]/[taskId]/_components/multimedia/MultimediaComponent.tsx
@@ -12,11 +12,7 @@ export default async function MultimediaComponent({ multimedia }: Props) {
   const url = await getMultimediaFileUrl(multimedia.multimedia_file_id)
   return (
     <div className={cx('cursor-default')}>
-      {multimedia.media_type === 'VIDEO' ? (
-        <Video src={url} />
-      ) : (
-        <Image alt="" draggable={false} src={url} width={1000} height={1000} />
-      )}
+      {multimedia.media_type === 'VIDEO' ? <Video src={url} /> : <img src={url} />}
     </div>
   )
 }
